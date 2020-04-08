5f376ea0262e705f05038883d98fd09d47ff8bd5
KarimeMartinez <61034288+KarimeMartinez@users.noreply.github.com>
2020-04-08 18:55:08 -0300

    AC02

---
+++ com/Karime/test/test_operacoes.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from com.karime.operacoes import Operacoes
+
+class TestOperacoes(TestCase):
+
+    def setUp(self):
+        self.operacoes = Operacoes()
+    
+    def test_soma(self):
+        self.assertEqual(self.operacoes.soma([11, 2]), 13,
+         "Deveria ser 13"
